64f8f0e00840c26bcdc4388b19d8c432d4899c7e
Maxim Petrov <maximpetrov2612@gmail.com>
2024-09-15 13:33:33 +0300

    Created '.groovy' file for the 'Deployment package creation' stage

---
+++ vars/deploymentPackageCreation.groovy
@@ -0,0 +1,16 @@
+def call(String packageName) {
+    echo "Creating deployment package: ${params.PACKAGE_NAME}"
+                        
+    powershell '''
+    Write-Host "Compressing files from: ${env:WORKSPACE}"
+    Write-Host "Saving to: ${env:PACKAGE_NAME}"
+
+    # Excludes the "Configuration" directory and ".gitmodules" file during compression.
+    $itemsToCompress = Get-ChildItem -Path $env:WORKSPACE -Recurse | Where-Object {
+    $_.FullName -notlike "*\\Configuration*" -and $_.FullName -notlike "*.gitmodules"}
+
+    # Lists the files to be compressed.
+    $itemsToCompress | ForEach-Object { Write-Host "Including: $($_.FullName)" }    
+    Compress-Archive -Path $itemsToCompress.FullName -DestinationPath $env:PACKAGE_NAME -Force -Verbose
+    '''
+}
