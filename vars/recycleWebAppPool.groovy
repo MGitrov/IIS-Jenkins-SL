35b9cd339930d102b3b4a770e6808e42b8cbf67b
Maxim Petrov <maximpetrov2612@gmail.com>
2024-09-15 15:55:39 +0300

    Created '.groovy' file for the 'Recycling web app pool' stage

--- vars/recycleWebAppPool.groovy
+++ vars/recycleWebAppPool.groovy
@@ -1,5 +1,5 @@
-def call(String webAppPool) {
-    echo "Recycling ${webAppPool} web app pool..."
+def call() {
+    echo "Recycling ${env.WEB_APP_POOL} web app pool..."
 
     powershell '''
     Restart-WebAppPool -Name "$env:WEB_APP_POOL"
